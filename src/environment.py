018f72ddfbfec17d2e9a70e0b9a0e2026730b548
AlexisGitHu <56341573+AlexisGitHu@users.noreply.github.com>
2024-02-26 22:27:38 +0100

    Multiple updates
    El agente de PPO ya es capaz de entrenar mediante una memoria.
    
    El valor de delta(B) ya se actualiza.
    
    El environment está completamente definido.
    
    La función de recompensa está definida, pero se tiene que modificar por ser lo contrario a lo que se necesita.

--- src/environment.py
+++ src/environment.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from random import choice as random_choice
 from dataframe_handler import get_dataframe_from_file
+import copy
 import time
 import sys
 
@@ -77,9 +78,9 @@ class HistoricData:
             )
         return state_vector
 
-    def adapt_state_vector_to_state(self, state_vector):
+    def adapt_state_vector_to_state(self, date_tuple):
         final_state_vector = [[], [], []]
-        for hour_vector in state_vector:
+        for hour_vector in date_tuple:
             final_state_vector[0].append(hour_vector[0])
             final_state_vector[1].append(hour_vector[1])
             final_state_vector[2].append(hour_vector[2])
@@ -91,8 +92,8 @@ class HistoricData:
         )
         return actual_final_state_vector
 
-    def get_next_value(self, month, day, hour):
-        state_vector = self.adapt_state_vector_to_state(
+    def get_next_value(self, month, day, hour, state_vector):
+        new_values = self.adapt_state_vector_to_state(
             [
                 random_choice(
                     self.date_value_possible_pairs[
@@ -105,16 +106,17 @@ class HistoricData:
                 )
             ]
         )
-        for i, vector in enumerate(self.actual_final_state_vector):
+        for i, vector in enumerate(state_vector):
             vector.pop(0)
-            vector.append(state_vector[i][0])
+            vector.append(new_values[i][0])
+        return state_vector
 
-    def get_following_state_vector(self):
+    def get_following_state_vector(self, state_vector):
         if self.actual_month == 0:
             self.actual_month = random_choice(self.month_choices)
             self.actual_day = random_choice(self.day_choices[self.actual_month])
             self.actual_hour = random_choice(self.hour_choices)
-            self.actual_final_state_vector = self.get_final_state_vector(
+            actual_final_state_vector = self.get_final_state_vector(
                 self.actual_month, self.actual_day, self.actual_hour
             )
         else:
@@ -134,10 +136,12 @@ class HistoricData:
             self.actual_month = next_month
             self.actual_day = next_day
             self.actual_hour = next_hour
-            self.get_next_value(self.actual_month, self.actual_day, self.actual_hour)
-
+            actual_final_state_vector = self.get_next_value(
+                self.actual_month, self.actual_day, self.actual_hour, state_vector
+            )
+            # print(f"Esto es none?: {actual_final_state_vector}")
         # print(self.actual_month, self.actual_day, self.actual_hour)
-        return self.actual_final_state_vector
+        return actual_final_state_vector
 
     def reset_actual_date(self):
         self.actual_month = 0
@@ -146,31 +150,51 @@ class HistoricData:
 
 
 class State:
-    def __init__(self, historic_data, trained_weeks=2, iteration_interval=24):
+    def __init__(
+        self,
+        historic_data,
+        maximum_battery_power=2200,
+        trained_weeks=2,
+        iteration_interval=24,
+    ):
         self.actual_net_power: np.float64 = 0
-        self.net_power_window: np.NDArray[np.float64] = np.zeros(23)
+        self.net_power_window: np.NDArray[np.float64] = np.zeros(23).tolist()
         self.actual_pv_power: np.float64 = 0
-        self.pv_power_window: np.NDArray[np.float64] = np.zeros(23)
+        self.pv_power_window: np.NDArray[np.float64] = np.zeros(23).tolist()
         self.actual_light_pvpc_power: np.float64 = 0
-        self.light_pvpc_window: np.NDArray[np.float64] = np.zeros(23)
-        self.battery_power = 0
+        self.light_pvpc_window: np.NDArray[np.float64] = np.zeros(23).tolist()
+
+        self.maximum_battery_power = maximum_battery_power
+        self.actual_battery_power = 0
         self.inverse_battery_power = 0
         self.historic_data = historic_data
 
+        self.state_vector = self.create_initial_state_vector()
         self.trained_week = trained_weeks
         self.iteration_interval = iteration_interval
         self.total = self.trained_week * self.iteration_interval
         self.hour_iterator = 0
 
-    def update(self, action):
-        pass
+    def create_initial_state_vector(self):
+        state_vector = [
+            self.net_power_window,
+            self.pv_power_window,
+            self.light_pvpc_window,
+            [self.actual_battery_power],
+        ]
+        state_vector[0].insert(0, self.actual_net_power)
+        state_vector[1].insert(0, self.actual_pv_power)
+        state_vector[2].insert(0, self.actual_light_pvpc_power)
+        return state_vector
 
     def print_batch_ended(self):
-        print("\n", sep=" ")
-        print(f"Terminal state ended with:")
-        print(f"    + PV generated: {self.actual_net_power} (kW)")
-        print(f"    + Demand: {self.actual_pv_power} (kWh)")
-        print(f"    + PVPC light: {self.actual_light_pvpc_power} (€/kWh)\n")
+        # print("\n", sep=" ")
+        # print(f"Terminal state ended with:")
+        # print(f"    + PV generated: {self.actual_net_power} (kW)")
+        # print(f"    + Demand: {self.actual_pv_power} (kWh)")
+        # print(f"    + PVPC light: {self.actual_light_pvpc_power} (€/kWh)")
+        # print(f"    + Battery power: {self.actual_battery_power} (kW)\n")
+        pass
 
     def iterate_batch(self):
         if self.hour_iterator == self.total:
@@ -179,9 +203,9 @@ class State:
             self.print_batch_ended()
         else:
             self.hour_iterator += 1
-            self.progress_bar(
-                self.hour_iterator,
-            )
+            # self.progress_bar(
+            #     self.hour_iterator,
+            # )
 
     def progress_bar(
         self,
@@ -208,80 +232,77 @@ class State:
         self.actual_light_pvpc_power = state_vector[2][0]
         self.light_pvpc_window = np.array(state_vector[2][1:])
 
-    def next_state(self, action=0):
-        state_vector = self.historic_data.get_following_state_vector()
+    def add_actual_battery_value(self, partial_state_vector):
+        partial_state_vector.append([self.actual_battery_power])
+        return partial_state_vector
+
+    def next_state(self):
+        partial_state_vector = self.historic_data.get_following_state_vector(
+            self.state_vector[:3]
+        )  # Historic data can only return 3 lists of values
+        state_vector = self.add_actual_battery_value(partial_state_vector)
         self.update_class_variables(state_vector)
         self.iterate_batch()
-
-    def get_next_power_window(self):
-        if self.hour_iterator == 0:
-            self.hour_iterator = 1
-            return self.net_power_window
-
-    def read_next_state_from_data(self):
-        pass
+        self.state_vector = copy.deepcopy(state_vector)
+        return state_vector
 
 
 class Environment:
     def __init__(
         self,
-        net_installed_power,
-        pv_installed_power,
-        battery_capacity,
-        inverse_battery_capacity,
         dataframe_historic_data,
+        alpha=0.6,
     ):
 
-        self.net_installed_power = net_installed_power
-        self.pv_installed_power = pv_installed_power
-        self.battery_capacity = battery_capacity
-        self.inverse_battery_capacity = inverse_battery_capacity
-        self.state = State(historic_data_file_path=dataframe_historic_data)
+        # self.net_installed_power = net_installed_power
+        # self.pv_installed_power = pv_installed_power
+        # self.battery_capacity = battery_capacity
+        # self.inverse_battery_capacity = inverse_battery_capacity
+        self.dataframe_historic_data = dataframe_historic_data
+        self.state = State(historic_data=dataframe_historic_data)
+        self.current_steps = 0
+        self.current_observation = None
+        self.alpha = alpha
 
-    def __compute_reward(self):
+    def flatten(self, xss):
+        return [x for xs in xss for x in xs]
+
+    def __compute_reward(self, action):
         # Compute the reward
-        reward = 0
+        p = self.state.state_vector[0][0]
+        f = self.state.state_vector[1][0]
+        reward = (
+            -p + f + action
+        ) * self.alpha  ## Esto es lo que hay que cambiar, converge a 0.
         return reward
 
     def step(self, action):
-        # Update the state
-        self.state.update(action)
+        if self.current_steps == 1024:
+            done = True
+            self.current_steps = 0
+        else:
+            done = False
+            self.current_steps += 1
+        reward = self.__compute_reward(action)
+        new_state = self.flatten(self.state.next_state())
 
-        # Compute the reward
-        reward = self._compute_reward()
-
-        return self.state, reward
+        return new_state, reward, done
 
     def reset(self):
-        self.state = None
+        self.state = State(historic_data=self.dataframe_historic_data)
+        self.current_observation = self.flatten(self.state.next_state())
+        return self.current_observation
 
 
 if __name__ == "__main__":
-    # Load the data
-    # net_installed_power = 100
-    # pv_installed_power = 100
-    # battery_capacity = 100
-    # inverse_battery_capacity = 100
-
-    # environment = Environment(
-    #     net_installed_power,
-    #     pv_installed_power,
-    #     battery_capacity,
-    #     inverse_battery_capacity,
-    #     "datos/clean/merged_data.csv",
-    # )
-
-    # # Train the agent
-    # for i in range(100):
-    #     action = random_choice([0, 1])
-    #     state, reward = environment.step(action)
-    #     print(f"Step {i}: {state}, {reward}")
-
-    # # Reset the environment
-    # environment.reset()
+    # history = HistoricData("datos/clean/merged_data.csv")
+    # train_weeks = 2
+    # iteration_interval = 24
+    # state = State(history, 2.2)
+    # for i in range(100000):
+    #     state.next_state()
     history = HistoricData("datos/clean/merged_data.csv")
-    train_weeks = 2
-    iteration_interval = 24
-    state = State(history)
+    env = Environment(history)
+    env.reset()
     for i in range(100000):
-        state.next_state()
+        env.step(0)
